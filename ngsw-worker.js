2b9e72b06421f659e22ebe7d2743774d372a8c1b
jaswanth03 <vjvr03@gmail.com>
2019-05-17 15:43:18 +0530

    service worker changed

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1981,26 +1981,11 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
             msg.waitUntil(this.handlePush(msg.data.json()));
         }
         onClick(event) {
-            // Handle the click event and keep the SW alive until it's handled.
-            let url = 'https://upfrontsecurity.github.io/#/home/alertDetails';
-            event.notification.close(); // Android needs explicit close.
+            event.notification.close();
+
             event.waitUntil(
-                clients.matchAll({type: 'window'}).then( windowClients => {
-                    // Check if there is already a window/tab open with the target URL
-                    for (var i = 0; i < windowClients.length; i++) {
-                        var client = windowClients[i];
-                        // If so, just focus it.
-                        if (client.url === url && 'focus' in client) {
-                            return client.focus();
-                        }
-                    }
-                    // If not, then open the target URL in a new window/tab.
-                    if (clients.openWindow) {
-                        return clients.openWindow(url);
-                    }
-                })
+                clients.openWindow('https://jaswanth03.github.io')
             );
-            // event.waitUntil(this.handleClick(event.notification, event.action));
         }
         handleMessage(msg, from) {
             return __awaiter$5(this, void 0, void 0, function* () {
